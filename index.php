4a347863dd588a6c294a6ad5e43622a711de1fb8
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-04-16 13:57:19 +0700

    membuat header dan navbar web

--- index.php
+++ index.php
@@ -6,10 +6,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPT Puskesmas Alianyang</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="responsive.css">
 </head>
 
 <body>
+    <div class="my-order">
+        <nav class="navbar navbar-expand-lg top-infos" data-bs-theme="dark">
+            <div class="container-fluid d-flex justify-content-center flex-wrap top-info">
+                <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentTop">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContentTop">
+                    <p class="d-inline-flex align-items-center mb-0 me-3 fw-light"><span class="material-symbols-sharp me-1">calendar_month</span>Senin, d-m</p>
+                    <p class="d-inline-flex align-items-center mb-0 me-3 fw-light"><span class="material-symbols-sharp me-1">schedule</span>Tutup dalam 30 menit</p>
+                    <p class="d-inline-flex align-items-center mb-0 fw-light"><span class="material-symbols-sharp me-1">mail</span>alianyang.pnkkota@gmail.com</p>
+                </div>
+            </div>
+        </nav>
 
+        <nav class="navbar navbar-expand-lg header" data-bs-theme="dark">
+            <div class="container">
+                <div>
+                    <img src="assets/images/logo-upt.png" alt="Puskesmas's logo" width="42">
+                    <a class="navbar-brand text-white" href="#">Puskesmas Alianyang</a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Pendaftaran</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Masuk</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!-- header ends -->
+    <!-- <header class="container-fluid header">
+        <div class="container d-flex justify-content-between align-items-center py-2">
+            <div class="left-header">
+                <img src="assets/images/logo-upt.png" alt="Logo Puskesmas" width="42">
+                <a href="#" class="text-black text-decoration-none text-white fw-medium">Puskesmas Alianyang</a>
+            </div>
+            <div class="right-header">
+                <a href="#" class="text-decoration-none text-white pe-4 fw-light test">Beranda</a>
+                <a href="#" class="text-decoration-none text-white pe-4 fw-light">Daftar Berobat</a>
+                <a href="#" class="text-decoration-none text-white fw-light">Masuk</a>
+            </div>
+        </div>
+    </header> -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
 </body>
 
 </html>
