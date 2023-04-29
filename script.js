28a6e76056c164fce1cc70f4d50170b05ca68503
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-04-29 15:25:31 +0700

    validasi jam di top info

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
 // awal menampilkan tanggal di top info
 const dateP = document.getElementById("tanggal");
-const spanDate = dateP.querySelector(".tanggal");
-const timerP = document.getElementById("timer");
-const spanTimer = timerP.querySelector(".timer");
-let today = new Date();
+const timeP = document.getElementById("timer");
 
 const months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-let day = days[today.getDay()];
-let date = today.getDate();
-let month = months[today.getMonth()];
-dateP.innerHTML += `${day}, ${date}-${month}`;
 
-function completeDate(h, m) {
-  let date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), h, m);
-  return date;
-}
+setInterval(() => {
+  let today = new Date();
+  // awal tanggal
+  let day = days[today.getDay()];
+  let date = today.getDate();
+  let month = months[today.getMonth()];
+  dateP.innerHTML = `${day}, ${date}-${month}`;
+  // akhir tanggal
 
-let dayOfWeek = today.getDay();
-let openTime, closeTime;
-if (dayOfWeek >= 1 && dayOfWeek <= 4) {
-  openTime = completeDate(7, 15);
-  closeTime = completeDate(12, 0);
-  console.log(openTime);
-} else if (dayOfWeek === 5 || dayOfWeek === 6) {
-  openTime = completeDate(7, 15);
-  closeTime = completeDate(11, 0);
-} else {
-  timerP.innerHTML += `Tutup`;
-  setInterval(() => {
-    timerP.innerHTML += `Tutup`;
-  }, 1000);
-}
+  // awal jam operasional
+  let time;
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let dayOfWeek = today.getDay();
+  let monthNumber = today.getMonth() + 1;
+  let url = `https://api-harilibur.vercel.app/api?month=${monthNumber}`;
 
-if (openTime && closeTime) {
-  let timeLeft = Math.floor((closeTime.getTime() - today.getTime()) / 1000);
-  if (timeLeft <= 0) {
-    timerP.innerHTML += `Tutup`;
-  } else {
-    let hoursLeft = Math.floor(timeLeft / 3600);
-    let minutesLeft = Math.floor((timeLeft % 3600) / 60);
-    let timeString = `Tutup dalam ${hoursLeft} jam ${minutesLeft} menit`;
-    timerP.innerHTML += timeString;
-    setInterval(() => {
-      timeLeft--;
-      if (timeLeft <= 0) {
-        timerP.innerHTML += `Tutup`;
-      } else {
-        hoursLeft = Math.floor(timeLeft / 3600);
-        minutesLeft = Math.floor((timeLeft % 3600) / 60);
-        timeString = `Tutup dalam ${hoursLeft} jam ${minutesLeft} menit`;
-        timerP.innerHTML += timeString;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let a = [];
+      for (let i = 0; i <= data.length - 1; i++) {
+        a[i] = new Date(data[i].holiday_date);
+        if (a[i].getDate() === today.getDate() && a[i].getMonth() === today.getMonth() && a[i].getFullYear() === today.getFullYear() && a[i].getDay() === 0) {
+          time = `Tutup Memperingati `;
+          time += data[i].holiday_name;
+          break;
+        } else if (dayOfWeek >= 1 && dayOfWeek <= 4) {
+          if (hours < 7 || (hours == 7 && minutes < 15) || hours >= 12) {
+            time = "Tutup a";
+          } else {
+            time = "07:15 - 12:00";
+          }
+        } else if (dayOfWeek == 5 || dayOfWeek == 6) {
+          if (hours < 7 || (hours == 7 && minutes < 15) || hours >= 11) {
+            time = "Tutup b";
+          } else {
+            time = "07:15 - 11:00";
+          }
+        } else if (dayOfWeek == 0) {
+          time = "Tutup c";
+        }
       }
-    }, 1000);
-  }
-}
+      timeP.innerHTML = time;
+    });
+  // akhir jam operasional
+}, 1000);
+
 // akhir menampilkan tanggal di top info
