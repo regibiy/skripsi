bbe7a3f67151a568e33a31533642a74be91a3719
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-05-12 17:19:40 +0700

    commit detail information

---
+++ information.php
@@ -0,0 +1,31 @@
+<?php
+$title = "Informasi Kegiatan";
+include("action.php");
+include("views/header.php");
+?>
+
+<!-- body starts  -->
+<div class="container my-mtb-body my-p-body shadow-sm border rounded" id="information">
+    <h2 class="fs-4 text-center text-dark-emphasis mb-3">Pemeriksaan Buah Hati</h2>
+    <div class="text-center border-bottom mb-4">
+        <p class="fs-6 text-dark-emphasis">Diunggah pada: 05-04-2023</p>
+    </div>
+    <div class="d-flex justify-content-lg-around justify-content-md-center justify-content-sm-center flex-wrap">
+        <div class="col-lg-4 col-sm-12 col-md-7 mb-4">
+            <img src="assets/images/1.jpg" alt="information's photo" class="img-fluid">
+        </div>
+        <div class="text-secondary col-lg-7 col-sm-12 col-md-12">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ducimus recusandae assumenda perferendis adipisci totam debitis necessitatibus perspiciatis. Corporis voluptatum veritatis voluptate ab dolore eligendi esse est tempora sapiente. Possimus nam ipsa delectus totam laboriosam quasi molestias dolor, facilis eveniet culpa dolorem, voluptatum nisi dolores fugit. Delectus eveniet accusamus praesentium. Vitae, quos delectus quisquam quo pariatur ab cumque, neque natus harum temporibus consectetur nesciunt? Blanditiis facilis vero alias veritatis eos neque praesentium, dicta dignissimos voluptatum doloribus aperiam nemo aut accusamus laboriosam iste, numquam distinctio nulla eveniet molestiae! Ad magni harum ducimus minus quidem doloremque aliquid voluptatibus aperiam! Voluptas et illo, quos quibusdam itaque provident nesciunt eius voluptatum ea fuga consequatur ex officiis cumque dolorem dolor tempora non praesentium, soluta doloribus atque rerum aliquid modi aut. Ipsam quod rerum at inventore aut facilis perspiciatis assumenda! Officiis, autem laboriosam a voluptas provident suscipit in quasi culpa amet explicabo commodi deleniti natus laborum nostrum! Deleniti consequatur nemo, illum tempore numquam voluptatum reprehenderit accusantium. Necessitatibus iste harum dolore quos qui? Laboriosam reiciendis illo dicta minima aliquam consectetur suscipit quas? Numquam expedita dolor quam at eos tenetur est reiciendis sunt deleniti fugit ut adipisci voluptate ducimus sequi laudantium aspernatur, illo odio, vitae ex saepe nisi quisquam in asperiores! Nam odio id animi. Velit reiciendis tenetur sit necessitatibus sunt excepturi exercitationem quo, porro eligendi blanditiis? Hic numquam, repellendus natus aliquid distinctio quisquam soluta quia nisi mollitia non est corporis voluptatum itaque amet minus. Dolorem deleniti aliquam asperiores temporibus mollitia laborum tempora, nesciunt harum quis minus repudiandae voluptate quasi nostrum reprehenderit quas doloremque ipsum cumque error blanditiis? Voluptas a exercitationem tempore reiciendis explicabo doloribus neque ab ex libero sapiente adipisci nesciunt repudiandae iste sunt veniam, sit iure? Fugiat porro non alias, perspiciatis architecto facilis consectetur cupiditate sed ab, tenetur voluptate rerum, modi voluptatum distinctio eum nisi ducimus.</p>
+            <p>Hari: Senin, 10 April 2023</p>
+            <p>Jam: 09:00 sampai selesai</p>
+            <p>Dokter: dr. Rosyadi Akbari M.Sc.Sp.A</p>
+        </div>
+    </div>
+    <div class="text-center border-top">
+        <p class="fs-6 text-dark-emphasis mb-0 mt-4">Diunggah oleh: pitaw123</p>
+    </div>
+</div>
+
+<!-- body ends -->
+<?php
+include("views/footer.php");
