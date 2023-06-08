83af4250fb456b47a1d5d1e5b7d7544d20dc0787
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-06-08 21:29:31 +0700

    menambah objek pendaftaran pasien

--- cores/Users.php
+++ cores/Users.php
@@ -2,9 +2,8 @@
 class Pasien
 {
     // properties
-    private $no_berobat;
-    private $nama;
-    private $password;
+    private $no_berobat, $no_kk, $nik, $nama_depan, $nama_belakang, $tempat_lahir, $tanggal_lahir,
+        $jenis_kelamin, $agama, $pekerjaan, $no_hp, $email, $alamat, $rt, $rw, $kel_desa, $kecamatan, $ktp_kk, $kk, $password;
     // method
     function set_login_data($no_berobat, $password)
     {
@@ -18,6 +17,116 @@ class Pasien
     function get_password()
     {
         return $this->password;
+    }
+
+    function set_daftar_data_identitas($no_kk, $nik, $nama_depan, $nama_belakang, $tempat_lahir, $tanggal_lahir)
+    {
+        $this->no_kk = $no_kk;
+        $this->nik = $nik;
+        $this->nama_depan = $nama_depan;
+        $this->nama_belakang = $nama_belakang;
+        $this->tempat_lahir = $tempat_lahir;
+        $this->tanggal_lahir = $tanggal_lahir;
+    }
+    function get_no_kk()
+    {
+        return $this->no_kk;
+    }
+    function get_nik()
+    {
+        return $this->nik;
+    }
+    function get_nama_depan()
+    {
+        return $this->nama_depan;
+    }
+    function get_nama_belakang()
+    {
+        return $this->nama_belakang;
+    }
+    function get_tempat_lahir()
+    {
+        return $this->tempat_lahir;
+    }
+    function get_tanggal_lahir()
+    {
+        return $this->tanggal_lahir;
+    }
+
+    function set_daftar_data_sosial($jenis_kelamin, $agama, $pekerjaan)
+    {
+        $this->jenis_kelamin = $jenis_kelamin;
+        $this->agama = $agama;
+        $this->pekerjaan = $pekerjaan;
+    }
+    function get_jenis_kelamin()
+    {
+        return $this->jenis_kelamin;
+    }
+    function get_agama()
+    {
+        return $this->agama;
+    }
+    function get_pekerjaan()
+    {
+        return $this->pekerjaan;
+    }
+
+    function set_daftar_data_kontak($no_hp, $email)
+    {
+        $this->no_hp = $no_hp;
+        $this->email = $email;
+    }
+    function get_no_hp()
+    {
+        return $this->no_hp;
+    }
+    function get_email()
+    {
+        return $this->email;
+    }
+
+    function set_daftar_data_domisili($alamat, $rt, $rw, $kel_desa, $kecamatan)
+    {
+        $this->alamat = $alamat;
+        $this->rt = $rt;
+        $this->rw = $rw;
+        $this->kel_desa = $kel_desa;
+        $this->kecamatan = $kecamatan;
+    }
+    function get_alamat()
+    {
+        return $this->alamat;
+    }
+    function get_rt()
+    {
+        return $this->rt;
+    }
+    function get_rw()
+    {
+        return $this->rw;
+    }
+    function get_kel_desa()
+    {
+        return $this->kel_desa;
+    }
+    function get_kecamatan()
+    {
+        return $this->kecamatan;
+    }
+
+    function set_data_pendukung($ktp_kk, $kk)
+    {
+        $this->ktp_kk = $ktp_kk;
+        $this->kk = $kk;
+    }
+    function get_ktp_kk()
+    {
+        return $this->ktp_kk;
+    }
+    function get_kk()
+    {
+        return $this->kk;
     }
 }
 
