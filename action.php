998f4eda0f48a4f4eab105adf33082f008eee5a8
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-06-09 09:40:54 +0700

    menambah script daftar akun pasien

--- action.php
+++ action.php
@@ -11,19 +11,16 @@ function check_status_login_pasien()
 
 function upload_file($file_name, $file_size, $temp_location)
 {
-    //cek extension file
     $valid_ext = ['jpg', 'jpeg', 'png'];
     $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
     if (!in_array($ext, $valid_ext)) {
         $_SESSION['error_msg'] = "Silakan masukkan foto KTP dengan ekstensi .jpg, .jpeg, atau .png";
         header("Location: account-registration.php");
     } else {
-        //limit file size
         if ($file_size > 3000000) {
             $_SESSION['error_msg'] = "Ukuran file harus kurang dari 3MB";
             header("Location: account-registration.php");
         } else {
-            echo $temp_location;
             $new_file_name = uniqid() . "." . $ext;
             move_uploaded_file($temp_location, 'assets/patient_data/' . $new_file_name);
             return $new_file_name;
@@ -63,29 +60,34 @@ if (isset($_POST['login'])) {
 if (isset($_POST['daftar_akun_pasien'])) {
     $pasien = new Pasien;
     $pasien->set_daftar_data_identitas($_POST['no_kk'], $_POST['nik'], $_POST['nama_depan'], $_POST['nama_belakang'], $_POST['tempat_lahir'], $_POST['tanggal_lahir']);
-    // var_dump($pasien->get_no_kk());
-    // var_dump($pasien->get_nik());
-    // var_dump($pasien->get_nama_depan());
-    // var_dump($pasien->get_nama_belakang());
-    // var_dump($pasien->get_tempat_lahir());
-    // var_dump($pasien->get_tanggal_lahir());
-
     $pasien->set_daftar_data_sosial($_POST['jenis_kelamin'], $_POST['agama'], $_POST['pekerjaan']);
-    // var_dump($pasien->get_jenis_kelamin());
-    // var_dump($pasien->get_agama());
-    // var_dump($pasien->get_pekerjaan());
-
     $pasien->set_daftar_data_kontak($_POST['no_hp'], $_POST['email']);
-    // var_dump($pasien->get_no_hp());
-    // var_dump($pasien->get_email());
-
     $pasien->set_daftar_data_domisili($_POST['alamat'], $_POST['rt'], $_POST['rw'], $_POST['kel_desa'], $_POST['kecamatan']);
-    // var_dump($pasien->get_alamat());
-    // var_dump($pasien->get_rt());
-    // var_dump($pasien->get_rw());
-    // var_dump($pasien->get_kel_desa());
-    // var_dump($pasien->get_kecamatan());
-
-    $ktp = upload_file($_FILES['ktp']['name'], $_FILES['ktp']['size'], $_FILES['ktp']['tmp_name']);
-    $kk = upload_file($_FILES['kk']['name'], $_FILES['kk']['size'], $_FILES['kk']['tmp_name']);
+    $no_kk = $pasien->get_no_kk();
+    $nik = $pasien->get_nik();
+    $email = $pasien->get_email();
+    $alamat = $pasien->get_alamat();
+    $rt = $pasien->get_rt();
+    $rw = $pasien->get_rw();
+    $kel_desa = $pasien->get_kel_desa();
+    $kecamatan = $pasien->get_kecamatan();
+    $sql = "SELECT * FROM akun WHERE no_kk = '$no_kk'";
+    $cek = $conn->query($sql);
+    if ($cek->num_rows > 0) {
+        $_SESSION['error_msg'] = "Nomor KK sudah terdaftar! Silakan ke <a href='forgot-account.php' class='text-decoration-none text-white fw-semibold'>halaman lupa akun</a> untuk dikirimkan nomor berobat dan kata sandi";
+        header("Location: account-registration.php");
+    } else {
+        $sql = "SELECT * FROM pasien WHERE nik = '$nik'";
+        $cek = $conn->query($sql);
+        if ($cek->num_rows > 0) {
+            $_SESSION['error_msg'] = "NIK Anda terikat dengan KK lain! Silakan ubah Status Pasien pada akun terikat terlebih dahulu";
+            header("Location: account-registration.php");
+        } else {
+            $ktp = upload_file($_FILES['ktp']['name'], $_FILES['ktp']['size'], $_FILES['ktp']['tmp_name']);
+            $kk = upload_file($_FILES['kk']['name'], $_FILES['kk']['size'], $_FILES['kk']['tmp_name']);
+            $sql = "INSERT INTO akun VALUES ('$no_kk', NULL, '$email', NULL, '$alamat', '$rt', '$rw', '$kel_desa', '$kecamatan', '$kk')";
+            $result_akun = $conn->query($sql);
+            if ($result) header("Location: succeed.php");
+        }
+    }
 }
