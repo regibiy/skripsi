608f7ecf04054cedad5fc82e58b4a907e41822ba
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-06-14 18:28:48 +0700

    menambah back end

--- forgot-account.php
+++ forgot-account.php
@@ -4,16 +4,24 @@ include("action.php");
 include("views/header.php");
 ?>
 
-<!-- body starts -->
-<div class="container d-flex justify-content-center my-mtb-body">
-    <div class="d-flex flex-column gap-4 col-lg-5 col-sm-12 col-md-7">
-        <label for="email" class="form-label mb-0 fs-6 text-dark-emphasis">Email yang terdaftar</label>
-        <input type="email" class="form-control fs-7 text-dark-emphasis" id="email" placeholder="pasien@domain.com">
-        <!-- <p class="mb-0 bg-danger text-white fs-7 p-1 rounded">Pesan keberhasilan dan kegagalan</p> -->
-        <button class="btn btn-sm btn-success fs-6">Kirim nomor berobat dan kata sandi</button>
+<form action="action.php" method="post">
+    <div class="container d-flex justify-content-center my-mtb-body">
+        <div class="d-flex flex-column gap-4 col-lg-5 col-sm-12 col-md-7">
+            <label for="email" class="form-label mb-0 fs-6 text-dark-emphasis">Email yang terdaftar</label>
+            <input type="email" class="form-control fs-7 text-dark-emphasis" id="email" name="email" placeholder="pasien@domain.com" maxlength="50" required>
+            <?php
+            if (isset($_SESSION['error_msg'])) {
+                echo "<p class='mb-0 bg-danger text-white fs-7 p-1 rounded'>" . $_SESSION['error_msg'] . "</p>";
+                unset($_SESSION['error_msg']);
+            } elseif (isset($_SESSION['success_msg'])) {
+                echo "<p class='mb-0 bg-success text-white fs-7 p-1 rounded'>" . $_SESSION['success_msg'] . "</p>";
+                unset($_SESSION['success_msg']);
+            }
+            ?>
+            <button type="submit" class="btn btn-sm btn-success fs-6" name="forgot-account">Kirim nomor berobat dan kata sandi</button>
+        </div>
     </div>
-</div>
-<!-- body ends -->
+</form>
 
 <?php
 include("views/footer.php");
