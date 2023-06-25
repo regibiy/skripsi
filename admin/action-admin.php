b9a4607384a265ee39b6148c012e6210a9383f31
Regi Ridho Biyantomo <regibiyantomo123@gmail.com>
2023-06-25 13:25:32 +0700

    commit sebelum nokk nik noindeks fix

--- admin/action-admin.php
+++ admin/action-admin.php
@@ -1,4 +1,7 @@
 <?php
+
+use Mpdf\Barcode\Postnet;
+
 include("../cores/function.php");
 include("../cores/connection.php");
 include("../cores/Users.php");
@@ -153,7 +156,7 @@ if (isset($_POST['tambah_informasi'])) {
     $judul = $informasi->get_judul();
     $tanggal_unggah = date('Y-m-d H:i:s');
     $username = $_SESSION['username'];
-    $deskripsi = $informasi->get_deskripsi();
+    $deskripsi = nl2br($informasi->get_deskripsi()); //agar format tetap
     $dokter = $informasi->get_dokter();
     $tanggal = $informasi->get_tanggal();
     $jam_mulai = $informasi->get_jam_mulai();
@@ -175,10 +178,11 @@ if (isset($_POST['tambah_informasi'])) {
 
 if (isset($_POST['edit_informasi'])) {
     $informasi = new Informasi_Kegiatan;
+    $id_informasi = $_POST['id_informasi'];
     $informasi->set_data_informasi($_POST['judul'], $_POST['deskripsi'], $_POST['dokter'], $_POST['tanggal'], $_POST['jam_mulai'], $_POST['jam_selesai']);
     $judul = $informasi->get_judul();
     $tanggal_ubah = date('Y-m-d H:i:s');
-    $deskripsi = $informasi->get_deskripsi();
+    $deskripsi = nl2br($informasi->get_deskripsi());
     $prev_gambar = $_POST['prev_gambar'];
     $tanggal = $informasi->get_tanggal();
     $jam_mulai = $informasi->get_jam_mulai();
@@ -191,7 +195,8 @@ if (isset($_POST['edit_informasi'])) {
         $gambar_edit = upload_file($_FILES['gambar']['name'], $_FILES['gambar']['size'], $_FILES['gambar']['tmp_name'], 'assets/images/');
     }
 
-    $sql = "UPDATE informasi SET judul = '$judul', tanggal_ubah = '$tanggal_ubah', deskripsi = '$deskripsi', gambar = '$gambar_edit', tanggal = '$tanggal', jam_mulai = '$jam_mulai', jam_selesai = '$jam_selesai', id_dokter = '$id_dokter'";
+    $sql = "UPDATE informasi SET judul = '$judul', tanggal_ubah = '$tanggal_ubah', deskripsi = '$deskripsi', gambar = '$gambar_edit', tanggal = '$tanggal', jam_mulai = '$jam_mulai', 
+    jam_selesai = '$jam_selesai', id_dokter = '$id_dokter' WHERE id_informasi = '$id_informasi'";
     $result = $conn->query($sql);
     if ($result) {
         echo "<script>
@@ -235,5 +240,180 @@ if (isset($_POST['edit_dokter'])) {
         alert('Data dokter berhasil diperbarui!');
         window.location = 'add-activity-registration.php';
         </script>";
+    }
+}
+
+if (isset($_POST['edit_status'])) {
+    $status = $_POST['status'];
+    $id_pendaftaran = $_POST['id_pendaftaran'];
+    $sql = "UPDATE pendaftaran SET status_pendaftaran = '$status' WHERE id_pendaftaran = '$id_pendaftaran'";
+    $result = $conn->query($sql);
+    if ($result) header("Location: index-registration.php");
+}
+
+if (isset($_POST['edit_status_rekmed'])) {
+    $status = $_POST['status'];
+    $id_pendaftaran =  $_POST['id_pendaftaran'];
+    $sql = "UPDATE pendaftaran SET status_pendaftaran = '$status' WHERE id_pendaftaran = '$id_pendaftaran'";
+    $result = $conn->query($sql);
+    if ($result) header("Location: all-registration-medical-record.php");
+}
+
+if (isset($_POST['set_tanggal_daftar'])) {
+    $tanggal_awal = $_POST['tanggal_awal'];
+    $tanggal_akhir = $_POST['tanggal_akhir'];
+    $enc_tanggal_awal = encrypt($tanggal_awal);
+    $enc_tanggal_akhir = encrypt($tanggal_akhir);
+    header("Location: all-registration.php?tanggalAwal=" . urlencode($enc_tanggal_awal) . "&tanggalAkhir=" . urlencode($enc_tanggal_akhir));
+}
+
+if (isset($_POST['set_tanggal_daftar_rekmed'])) {
+    $tanggal_awal = $_POST['tanggal_awal'];
+    $tanggal_akhir = $_POST['tanggal_akhir'];
+    $enc_tanggal_awal = encrypt($tanggal_awal);
+    $enc_tanggal_akhir = encrypt($tanggal_akhir);
+    header("Location: all-registration-medical-record.php?tanggalAwal=" . urlencode($enc_tanggal_awal) . "&tanggalAkhir=" . urlencode($enc_tanggal_akhir));
+}
+
+if (isset($_POST['cetak_laporan'])) {
+    $tanggal_awal = $_POST['tanggal_awal'];
+    $tanggal_akhir = $_POST['tanggal_akhir'];
+    $enc_tanggal_awal = encrypt($tanggal_awal);
+    $enc_tanggal_akhir = encrypt($tanggal_akhir);
+    header("Location: print-indirect-contact-registration.php?tanggalAwal=" . urlencode($enc_tanggal_awal) . "&tanggalAkhir=" . urlencode($enc_tanggal_akhir));
+}
+
+if (isset($_POST['selesai_daftar'])) {
+    $id_daftar = $_POST['id_daftar'];
+    $sql = "UPDATE pendaftaran SET status_pendaftaran = 'Selesai' WHERE id_pendaftaran = '$id_daftar'";
+    $result = $conn->query($sql);
+    if ($result) {
+        echo "<script>
+        alert('Pendaftaran berhasil diselesaikan! Kontrol semua data pendaftaran pasien pada menu Pendaftaran');
+        window.location = 'index-medical-record.php';
+        </script>";
+    }
+}
+
+if (isset($_POST['edit_data_kk_rekmed'])) {
+    $akun = new Akun;
+    $akun->set_data_akun($_POST['no_kk'], $_POST['no_indeks'], $_POST['password']);
+    $no_kk = $akun->get_no_kk();
+    $no_indeks = $akun->get_no_indeks();
+    $password = $akun->get_kata_sandi();
+    $akun->set_daftar_data_domisili($_POST['alamat'], $_POST['rt'], $_POST['rw'], $_POST['kelurahan_desa'], $_POST['kecamatan']);
+    $alamat = $akun->get_alamat();
+    $rt = $akun->get_rt();
+    $rw = $akun->get_rw();
+    $kel_desa = $akun->get_kel_desa();
+    $kecamatan = $akun->get_kecamatan();
+    $no_kk_prev = $_POST['no_kk_prev'];
+    $email = $_POST['email'];
+
+    if ($no_kk === $no_kk_prev) $url_kk = $no_kk_prev;
+    else $url_kk = $no_kk;
+    $enc_no_kk = encrypt($url_kk);
+
+    $sql = "UPDATE akun SET no_kk = '$no_kk', no_indeks = '$no_indeks', kata_sandi = '$password', 
+    alamat = '$alamat', rt = '$rt', rw = '$rw', kelurahan_desa = '$kel_desa', kecamatan = '$kecamatan' WHERE no_kk = '$no_kk_prev'";
+    $result = $conn->query($sql);
+    if ($result) {
+        $sql = "SELECT pasien.nik, nama_depan, nama_belakang, no_rekam_medis, no_kk FROM pasien LEFT JOIN rekam_medis ON pasien.nik = rekam_medis.nik 
+        WHERE no_kk = '$url_kk' AND status_hubungan = 'Kepala Keluarga'"; //get nik, no rekam medis, nama pasien 
+        $result = $conn->query($sql);
+        $data = $result->fetch_assoc();
+        $nik = $data['nik'];
+        $no_rekam_medis = $data['no_rekam_medis'];
+        $nama = $data['nama_depan'] . " " . $data['nama_belakang'];
+        // validasi rekam medis
+        if ($no_rekam_medis === NULL || $no_rekam_medis === "") {
+            $no_rekam_medis = "01" . $no_indeks;
+            $sql = "INSERT INTO rekam_medis (no_rekam_medis, nik) VALUES ('$no_rekam_medis', '$nik')";
+            $result = $conn->query($sql);
+            if ($result) {
+                $petugas = $_SESSION['name'];
+                if ($_SESSION['role'] === "rekmed") $bagian = "Rekam Medis";
+                // script code send mail
+                $title = "Nomor Berobat dan Kata Sandi Anda";
+                $content = "Hai, <b>" . $nama . "</b><br><br><br>
+                Terima kasih telah sabar menunggu.<br>Berikut adalah informasi akun Anda:<br><br>
+                Nomor Berobat : <b>" . $no_indeks . "</b><br> Kata Sandi : <b>" . $password . " </b><br><br>
+                Dengan masuk menggunakan akun di atas, Anda dapat melakukan pendaftaran rawat jalan secara online dan mengatur data anggota keluarga Anda.<br><br>
+                Jangan khawatir jika Anda lupa dengan nomor berobat dan kata sandi Anda, Anda dapat meminta sistem untuk mengirimkannya melalui <i>email</i> yang Anda gunakan
+                untuk mendaftar sebagai pasien baru di puskesmasalianyangpnk.my.id. 
+                <br><br><br>
+                Terima kasih.<br><br> Salam, <br>" . $petugas . ", Petugas " . $bagian . " <b>UPT Puskesmas Alianyang</b><br>
+                Jalan Pangeran Natakusuma, Pontianak Kota<br><br><br><i>*pesan dikirim oleh sistem, harap TIDAK membalas.</i>";
+                sendMail($email, $nama, $title, $content);
+                $_SESSION['success_msg'] = "No indeks dan kata sandi berhasil disimpan dan dikirim ke pasien menggunakan email yang tertera";
+                header("Location: detail-patient-medical-record.php?noKk=" . urlencode($enc_no_kk));
+            }
+        } else {
+            $_SESSION['success_msg'] = "Data nomor kepala keluarga berhasil diperbarui";
+            header("Location: detail-patient-medical-record.php?noKk=" . urlencode($enc_no_kk));
+        }
+    }
+}
+
+if (isset($_POST['edit_rekam_medis'])) {
+    $no_rekam_medis = $_POST['no_rekam_medis'];
+    $tanggal_masuk = $_POST['tanggal_masuk'];
+    $riwayat_alergi_obat = $_POST['riwayat_alergi_obat'];
+    $sql = "UPDATE rekam_medis SET tanggal_masuk = '$tanggal_masuk', riwayat_alergi_obat = '$riwayat_alergi_obat' WHERE no_rekam_medis = '$no_rekam_medis'";
+    $result = $conn->query($sql);
+    $enc_no_rekmed = encrypt($no_rekam_medis);
+    if ($result) {
+        $_SESSION['success_msg'] = "Data nomor rekam medis berhasil diperbarui";
+        header("Location: detail-patient-medical-record.php?noRekmed=" . urlencode($enc_no_rekmed));
+    }
+}
+
+if (isset($_POST['edit_data_nik_rekmed'])) {
+    $pasien = new Pasien;
+    $pasien->set_daftar_data_identitas($_POST['no_kk'], $_POST['nik'], $_POST['nama_depan'], $_POST['nama_belakang'], $_POST['tempat_lahir'], $_POST['tanggal_lahir']);
+    $no_kk = $pasien->get_no_kk();
+    $nik = $pasien->get_nik();
+    $nama_depan = $pasien->get_nama_depan();
+    $nama_belakang = $pasien->get_nama_belakang();
+    $tempat_lahir = $pasien->get_tempat_lahir();
+    $tanggal_lahir = $pasien->get_tanggal_lahir();
+    $pasien->set_daftar_data_sosial($_POST['jenis_kelamin'], $_POST['agama'], $_POST['pekerjaan']);
+    $jenis_kelamin = $pasien->get_jenis_kelamin();
+    $agama = $pasien->get_agama();
+    $pekerjaan = $pasien->get_pekerjaan();
+    $status_hubungan = $_POST['status_hubungan'];
+    $status_pasien = $_POST['status_pasien'];
+    $nik_prev = $_POST['nik_prev'];
+    $status_hubungan_prev = $_POST['status_hubungan_prev'];
+    $no_rekmed_prev = $_POST['no_rek_med_prev'];
+
+    if ($status_hubungan !== $status_hubungan_prev) {
+        $kode_rekmed = array("Kepala Keluarga" => "00", "Istri" => "10", "Anak 1" => "01", "Anak 2" => "02", "Anak 3" => "03", "Anak 4" => "04", "Anak 5" => "05", "Anak 6" => "06", "Anak 7" => "07", "Anak 8" => "08", "Anak 9" => "09");
+        foreach ($kode_rekmed as $key => $value) {
+            if ($status_hubungan === $key) $set_status_hubungan = $value;
+        }
+        $no_rekmed = $set_status_hubungan;
+        $no_rekmed .= substr($no_rekmed_prev, 2, 6);
+        var_dump($no_rekmed);
+        var_dump($no_rekmed_prev);
+        $sql = "SELECT * FROM rekam_medis WHERE no_rekam_medis = '$no_rekmed'"; // mencari data yang sudah ada
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $_SESSION['error_msg'] = "Status hubungan telah ada dalam data anggota keluarga pasien";
+            $enc_nik = encrypt($nik_prev);
+            header("Location: edit-patient-medical-record.php?nik=" . urlencode($enc_nik));
+        } else {
+            $sql = "UPDATE rekam_medis SET no_rekam_medis = '$no_rekmed' WHERE no_rekam_medis = '$no_rekmed_prev'";
+            $result = $conn->query($sql);
+        }
+    } else {
+        $sql = "UPDATE pasien SET nik = '$nik', nama_depan = '$nama_depan', nama_belakang = '$nama_belakang', tempat_lahir = '$tempat_lahir', tanggal_lahir = '$tanggal_lahir', 
+        jenis_kelamin = '$jenis_kelamin', agama = '$agama', pekerjaan = '$pekerjaan', status_hubungan = '$status_hubungan', status_pasien = '$status_pasien' WHERE nik = '$nik_prev'";
+        $result = $conn->query($sql);
+        if ($result) {
+            $_SESSION['success_msg'] = "Data Pasien berhasil diperbarui";
+            $enc_no_kk = encrypt($no_kk);
+            header("Location: detail-patient-medical-record.php?noKk=" . urlencode($enc_no_kk));
+        }
     }
 }
